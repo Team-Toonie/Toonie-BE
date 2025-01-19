7d49d03557346306161a8d56d7b0587f33261885
GeunHye <wheeinjj417@gmail.com>
2025-01-19 16:39:48 +0900

    -first commit

---
+++ src/main/java/com/example/toonieproject/ToonieprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.toonieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonieprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonieprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toonieproject/ToonieprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toonieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonieprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
