99f342da4cb6f0ae11c9552146d53ebf29fb079e
GeunHye <wheeinjj417@gmail.com>
2025-02-20 16:14:33 +0900

    -오류 수정: cors요청 허용

--- src/main/java/com/example/toonieproject/controller/Store/StoreController.java
+++ src/main/java/com/example/toonieproject/controller/Store/StoreController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/stores")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StoreController {
 
     private final StoreService storeService;
