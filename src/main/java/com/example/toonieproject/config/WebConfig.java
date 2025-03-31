6324b6b5c6769d714ea36b938c20c5ffd81b1aea
GeunHye <wheeinjj417@gmail.com>
2025-03-31 20:29:27 +0900

    - http변경

--- src/main/java/com/example/toonieproject/config/WebConfig.java
+++ src/main/java/com/example/toonieproject/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:8443", "https://localhost:5173")
+                .allowedOrigins("http://localhost:8443", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 최소한의 HTTP 메서드 허용
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 허용
