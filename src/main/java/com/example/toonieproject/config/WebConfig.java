6d6bed2e8815a848175bb44d62205dd97d54e0e8
crolvlee <leecrolv@gmail.com>
2025-07-10 22:22:05 +0900

    사진 관련

--- src/main/java/com/example/toonieproject/config/WebConfig.java
+++ src/main/java/com/example/toonieproject/config/WebConfig.java
@@ -1,20 +1,33 @@
 package com.example.toonieproject.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:8443", "https://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 최소한의 HTTP 메서드 허용
+                .allowedOrigins("https://localhost:8443", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true) // 쿠키 인증 허용
-                .maxAge(3600); // Preflight 요청을 1시간 동안 캐싱
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, octetStreamReadMsgConverter);
     }
 }
--- src/main/java/com/example/toonieproject/controller/Store/StoreController.java
+++ src/main/java/com/example/toonieproject/controller/Store/StoreController.java
@@ -5,8 +5,15 @@ import com.example.toonieproject.dto.Store.*;
 import com.example.toonieproject.entity.Store.Store;
 import com.example.toonieproject.service.Store.MapService;
 import com.example.toonieproject.service.Store.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +30,48 @@ public class StoreController {
     private final StoreService storeService;
     private final MapService mapService;
 
+//    @PreAuthorize("hasRole('OWNER')")
+//    @PostMapping("/add")
+//    public ResponseEntity<String> addStore(
+//            @RequestPart("request") AddStoreRequest request,
+//            @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+//
+//        Store store = storeService.add(request, imageFile);
+//
+//        // 리다이렉트 추가
+////        return ResponseEntity.status(HttpStatus.FOUND) // 302 Redirect
+////                .location(URI.create("/stores/{storeId}")) // 리다이렉트할 경로
+////                .build();
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body("Store created successfully");
+//
+//    }
+    @Operation(summary = "가게 등록!", description = "가게를 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "가게 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
+    @PostMapping(value = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("hasRole('OWNER')")
-    @PostMapping("/add")
     public ResponseEntity<String> addStore(
+
+            @Parameter(
+                    description = "가게 등록 요청 정보(JSON)",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AddStoreRequest.class)
+                    )
+            )
             @RequestPart("request") AddStoreRequest request,
-            @RequestPart(value = "image", required = false) MultipartFile imageFile) {
 
+            @Parameter(description = "가게 이미지 파일")
+            @RequestPart(value = "image", required = false) MultipartFile imageFile
+    ) {
         Store store = storeService.add(request, imageFile);
-
-        // 리다이렉트 추가
-//        return ResponseEntity.status(HttpStatus.FOUND) // 302 Redirect
-//                .location(URI.create("/stores/{storeId}")) // 리다이렉트할 경로
-//                .build();
-
         return ResponseEntity.status(HttpStatus.CREATED).body("Store created successfully");
-
     }
+
+
 
     @PreAuthorize("hasRole('OWNER')")
     @GetMapping("/owners/{ownerId}")
