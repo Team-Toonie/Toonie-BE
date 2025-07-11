9859c599f14c6689e0e37f22be06bacdccaa6264
GeunHye <wheeinjj417@gmail.com>
2025-07-11 16:01:00 +0900

    - firebase storage 연결 수정

--- src/main/java/com/example/toonieproject/config/WebConfig.java
+++ src/main/java/com/example/toonieproject/config/WebConfig.java
@@ -2,11 +2,20 @@ package com.example.toonieproject.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 허용
                 .maxAge(3600); // Preflight 요청을 1시간 동안 캐싱
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, octetStreamReadMsgConverter);
     }
 }
